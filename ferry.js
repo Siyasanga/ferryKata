39d5aa203f99c9d3bb391d531cd0955b7a74563a
Siyasanga <siyasanga@live.co.za>
2017-11-01 18:48:54 +0200

    Change onBoard datatype to array type

--- ferry.js
+++ ferry.js
@@ -2,28 +2,26 @@
 function ferry(carLimit,peopleLimit){
   var carCapacity = carLimit;
   var peopleCapacity = peopleLimit;
-  var onBoard = { peopleOnboard:0, carsOnboard:0};
+  this.onBoard = { peopleOnboard:0, carsOnboard:[]};
   var id = 1
   this.board = function(car){
-    if (onBoard.peopleOnboard+car.passangers <= peopleCapacity && onBoard.carsOnboard+1 <= carCapacity) {
-      if(typeof(onBoard[car.color]) == 'undefined'){
-        onBoard[car.color] = {};
-        onBoard[car.color][car.passangers] = {};
-        onBoard[car.color][car.passangers] = 1;
-        onBoard.carsOnboard++;
-        onBoard.peopleOnboard += car.passangers;
-        console.log(onBoard);
+    if (this.onBoard.peopleOnboard+car.passangers <= peopleCapacity && this.onBoard.carsOnboard+1 <= carCapacity) {
+      if(typeof(this.onBoard[car.color]) == 'undefined'){
+        this.onBoard[car.color] = {};
+        this.onBoard[car.color][car.passangers] = {};
+        this.onBoard[car.color][car.passangers] = 1;
+        this.onBoard.carsOnboard.push(JSON.stringify(car));
+        this.onBoard.peopleOnboard += car.passangers;
         return "accepted";
       }else {
-        if(typeof(onBoard[car.color][car.passangers]) == "undefined"){
-          onBoard[car.color][car.passangers] = 1;
-          onBoard.carsOnboard++;
+        if(typeof(this.onBoard[car.color][car.passangers]) == "undefined"){
+          this.onBoard[car.color][car.passangers] = 1;
+          this.onBoard.carsOnboard.push(JSON.stringify(car));
           return "accepted";
         }else {
-          onBoard[car.color][car.passangers]++;
-          console.log(onBoard);
-          if(onBoard[car.color][car.passangers]%5 == 0) return "half price!";
-          else if(onBoard[car.color][car.passangers]%7 == 0) return "you go free!"
+          this.onBoard.carsOnboard.push(JSON.stringify(car));
+          if(this.onBoard[car.color][car.passangers]%5 == 0) return "half price!";
+          else if(this.onBoard[car.color][car.passangers]%7 == 0) return "you go free!"
           else return "accepted";
         }
       }
@@ -33,6 +31,7 @@ function ferry(carLimit,peopleLimit){
   };
   // return
 }
+module.exports = ferry;
 var newFerry = new ferry(10,15);
 console.log(newFerry.board({color:"blue", passangers:4}));
 console.log(newFerry.board({color:"blue", passangers:4}));
@@ -42,3 +41,4 @@ console.log(newFerry.board({color:"blue", passangers:4}));
 console.log(newFerry.board({color:"blue", passangers:4}));
 console.log(newFerry.board({color:"blue", passangers:4}));
 console.log(newFerry.board({color:"red", passangers:4}));
+console.log(newFerry.onBoard);
